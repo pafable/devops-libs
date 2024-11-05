9541f72359429384f53f0b8f1013c4bfc772cbd7
pafable <philafable@gmail.com>
2024-11-05 07:15:32 -0500

    removed

--- src/com/pafable/PipelineArgs.groovy
+++ src/com/pafable/PipelineArgs.groovy
@@ -1,8 +1,12 @@
 package com.pafable
 
 class PipelineArgs {
-    public static final ArrayList<String> taskNames = [
+    public static final ArrayList<String> taskActions = [
         'create',
         'destroy'
     ]
+
+    public static final string taskDesc = 'Create or destroy actions'
+
+    public static final string taskName = 'ACTIONS'
 }
