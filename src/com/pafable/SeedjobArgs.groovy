b4322241b5f7b215bcac78479b7c7e377aec72c4
pafable <philafable@gmail.com>
2024-12-16 10:51:51 -0500

    added SeedjobArgs

--- src/com/pafable/SeedjobArgs.groovy
+++ src/com/pafable/SeedjobArgs.groovy
@@ -1,8 +1,11 @@
 package com.pafable
 
+import groovy.transform.TypeChecked
+
 final String platformBranch = 'refs/heads/master'
 final String platformRepo = 'https://github.com/pafable/k8s-platform.git'
 
+@TypeChecked
 enum SeedjobArgs {
     APPS_DEPLOYER(
         'apps-deployer',
